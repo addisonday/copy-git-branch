3ac2ef0bd896e929f6ec80fea9fa8c4a131a9dfe
Addison <day.addison@yahoo.com>
2018-02-28 23:16:36 -0800

    Update cbr.sh

--- cbr.sh
+++ cbr.sh
@@ -2,7 +2,7 @@
 # Simple script to copy git branch name to clipboard using xclip
 
 # git branch  - log branch to terminal
-# 2>/dev/null - discard output of command
+# 2>/dev/null - discard error
 # grep "^*"   - match output
 # colrm 1 2   - removes 1st - 2nd characters
 # xclip -i -selection clipboard - copy output to clipboard
